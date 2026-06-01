6cc37d40fb0d708e590203b374dc39002023a71a
lemonllhon <187522719+lemonllhon@users.noreply.github.com>
2026-06-01 14:29:30 +0000

    chore(homebrew): update cask for v0.0.31

--- Casks/cockpit-tools.rb
+++ Casks/cockpit-tools.rb
@@ -1,6 +1,6 @@
 cask "cockpit-tools" do
-  version "0.0.29"
-  sha256 "691fc7e3809803167bb97016eb899586858585c83e42e88fb45206adf9e5e6fc"
+  version "0.0.31"
+  sha256 "a7e963d576b8c56dcbe7785c3122fdd1d5e04ebe6fd70b46591c95cdf9209a2d"
 
   url "https://github.com/lemon-casino/ai-lemon-tools-release/releases/download/#{version}/AI.Lemon.Tools_#{version}_universal.dmg",
       verified: "https://github.com/lemon-casino/ai-lemon-tools-release/"
