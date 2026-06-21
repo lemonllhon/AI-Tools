b97037ae5ef24a8ccef471ec70f4404173684077
lemonllhon <187522719+lemonllhon@users.noreply.github.com>
2026-06-21 18:18:17 +0000

    chore(homebrew): update cask for v0.0.61

--- Casks/ai-lemon-tools.rb
+++ Casks/ai-lemon-tools.rb
@@ -1,6 +1,6 @@
 cask "ai-lemon-tools" do
-  version "0.0.60"
-  sha256 "846ce766b84a9f3637a6502a16690cce70a319d1164f563a1b66fecabea1149e"
+  version "0.0.61"
+  sha256 "ca2dc3cf76616471acf89d8078ac214d927fbca40d683206f98b0ec9074fbce6"
 
   url "https://github.com/lemon-casino/ai-lemon-tools-release/releases/download/#{version}/AI.Lemon.Tools_#{version}_universal.dmg",
       verified: "https://github.com/lemon-casino/ai-lemon-tools-release/"
